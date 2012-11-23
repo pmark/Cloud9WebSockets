c5268029b0cf4cbd2472e0f3547fd4f4cf5c6674
P. Mark Anderson <mark@bordertownlabs.com>
2012-11-23 15:04:27 -0500

    install

--- node.js/server.js
+++ node.js/server.js
@@ -1,14 +1,45 @@
 // reference the http module so we can create a webserver
-var http = require("http");
+//var http = require("http");
 
 // create a server
-http.createServer(function(req, res) {
-    // on every request, we'll output 'Hello world'
-    res.end("Hello world from Cloud9!");
-}).listen(process.env.PORT, process.env.IP);
+//http.createServer(function(req, res) {
+//    // on every request, we'll output 'Hello world'
+//    res.end("Hello world from Cloud9!");
+//}).listen(process.env.PORT, process.env.IP);
 
 // Note: when spawning a server on Cloud9 IDE, 
 // listen on the process.env.PORT and process.env.IP environment variables
 
 // Click the 'Run' button at the top to start your server,
 // then click the URL that is emitted to the Output tab of the console
+
+
+var app = require('http').createServer(handler)
+, io = require('socket.io').listen(app)
+, fs = require('fs')
+
+app.listen(process.env.PORT || 8001);
+
+function handler (req, res) {
+    fs.readFile('index.html',
+
+    function (err, data) {
+        if (err) {
+            res.writeHead(500);
+            return res.end('Error loading index.html');
+        }
+
+        res.writeHead(200, {'Content-Type': 'text/html', "Content-Length": data.length});
+        res.end(data);
+    });
+}
+
+io.sockets.on('connection', function (socket) {
+    // echo the message
+    socket.on('message', function (data) {
+        console.info(data);
+        socket.send("[ECHO] "+data);
+    });
+});
+
+
